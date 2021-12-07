7f5bf97371424b414d74638171d5a6958b3178df
jitpz <georgies1me@gmail.com>
2021-12-07 19:20:09 -0300

    Define shared module
    - Shared module will host components, pipes, directives, etc meant to be
     reused in other modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@app/core.module';
+import { SharedModule } from '@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
+    SharedModule,
     AppRoutingModule,
     BrowserAnimationsModule,
   ],
