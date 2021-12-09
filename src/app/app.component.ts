41b41c26c41f7387f7f02ff491d583a335701716
jitpz <georgies1me@gmail.com>
2021-12-09 00:59:15 -0300

    Define layout
    - Header and sidenav components were added.
    - Home component template changed to show nav buttons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'star-wars-explorer';
+  title = 'Star wars explorer';
 }
